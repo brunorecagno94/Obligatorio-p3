fb68a3a097503c54e89118d71e1353ed0ab546f1
Bruno Recagno <brunorecagno@gmail.com>
2025-05-07 20:28:03 -0300

    Obligatorio 1: agregada lista de Comentarios en Envio y ajuste de VO Estado

--- Obligatorio.LogicaNegocio/Entidades/Envio.cs
+++ Obligatorio.LogicaNegocio/Entidades/Envio.cs
@@ -11,22 +11,20 @@ namespace Obligatorio.LogicaNegocio.Entidades
         public Cliente Cliente { get; set; }
         public PesoPaquete PesoPaquete { get; set; }
         public NumeroTracking NumeroTracking { get; set; }
-        public Comentario Comentario { get; set; }
+        public List<Comentario> ListaComentario { get; set; }
         public DateTime FechaSalida { get; set; }
         public DateTime FechaLlegada { get; set; }
-        public Estado Estado { get; set; }
-        //public Estado Estado { get; set; } = Estado.EnProceso;
+        public Estado Estado { get; set; } = Estado.EnProceso;
 
 
-        public Envio(Empleado empleado, Cliente cliente, PesoPaquete pesoPaquete, NumeroTracking numeroTracking, Comentario comentario)
+        public Envio(Empleado empleado, Cliente cliente, PesoPaquete pesoPaquete, NumeroTracking numeroTracking)
         {
             Empleado = empleado;
             Cliente = cliente;
             PesoPaquete = pesoPaquete;
             NumeroTracking = numeroTracking;
-            Comentario = comentario;
             FechaSalida = DateTime.Now;
-            //Estado = Estado.EnProceso;
+            Estado = Estado.EnProceso;
         }
 
         public bool Equals(Envio? obj)
--- Obligatorio.LogicaNegocio/VO/Estado.cs
+++ Obligatorio.LogicaNegocio/VO/Estado.cs
@@ -1,23 +1,22 @@
-﻿using Obligatorio.LogicaNegocio.Excepciones;
-
-namespace Obligatorio.LogicaNegocio.VO
+﻿namespace Obligatorio.LogicaNegocio.VO
 {
     public record Estado
     {
-        //public static readonly Estado EnProceso = new Estado("EnProceso");
-        //public static readonly Estado Finalizado = new Estado("Finalizado");
+        public static readonly Estado EnProceso = new Estado("EnProceso");
+        public static readonly Estado Finalizado = new Estado("Finalizado");
 
         public string Value { get; }
 
-        public Estado(string value)
+        private Estado(string value)
         {
             Value = value;
-            Validar();
+            //Validar();
         }
 
-        public void Validar() {
+        //public void Validar()
+        //{
 
-            if (string.IsNullOrEmpty(Value)) throw new EstadoException("Estado inválido");
-        }
+        //    if (string.IsNullOrEmpty(Value)) throw new EstadoException("Estado inválido");
+        //}
     }
 }
