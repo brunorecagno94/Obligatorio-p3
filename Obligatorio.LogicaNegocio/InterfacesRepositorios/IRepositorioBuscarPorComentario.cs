85396bc764113af44fac05c002ae6603afbc9b5a
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2025-06-23 22:56:15 -0300

    Obligatorio2: Endpoint buscar por comentario y controller webapi

---
+++ Obligatorio.LogicaNegocio/InterfacesRepositorios/IRepositorioBuscarPorComentario.cs
@@ -0,0 +1,9 @@
+﻿using Obligatorio.LogicaNegocio.Entidades;
+
+namespace Obligatorio.LogicaNegocio.InterfacesRepositorios
+{
+    public interface IRepositorioBuscarPorComentario
+    {
+        IEnumerable<Envio> BuscarPorComentario(int idCliente, string palabraClave);
+    }
+}
